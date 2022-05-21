85e24aa148cee4eafd317bdd40419259481fe59f
Mateusz <alfagruis@gmail.com>
2022-05-21 15:52:33 +0200

    połowa wypożyczeń działa

--- src/app/home/hireMenu/hire/hire.page.ts
+++ src/app/home/hireMenu/hire/hire.page.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {FooterService} from '../../../_services/footer.service';
 import {QrcodeService} from "../../../_services/qrcode.service";
 import {ApiService} from "../../../_services/api.service";
+import {ToastController} from "@ionic/angular";
 
 @Component({
   selector: 'app-hire',
@@ -11,16 +12,35 @@ import {ApiService} from "../../../_services/api.service";
 export class HirePage implements OnInit {
   public hireTime = '31';
   public elementID = '';
+  public rules = false;
   customPopoverOptions: any = {
     header: 'Wybierz czas wypożyczenia',
     //subHeader: 'Select your hair color',
     message: 'W przypadku nie oddania na czas, istnieje możliwości przydłużenia wypożyczenia'
   };
 
-  constructor(public _footer: FooterService, private qrCode: QrcodeService, private api: ApiService) {
+  constructor(public _footer: FooterService,
+              private qrCode: QrcodeService,
+              private api: ApiService,
+              public toastController: ToastController) {
   }
 
   ngOnInit() {
+  }
+
+  async HireAccept(): Promise<void> {
+    if (this.rules === false) {
+      const toast = await this.toastController.create({
+        message: 'Aby wyporzyczyć element musisz zaakceptować regulamin',
+        duration: 700,
+        position: 'top'
+      });
+      toast.present();
+      return;
+    }
+    this.api.getDefault('hire/' + this.elementID + '/' + this.hireTime).then(dane => {
+      console.log(dane);
+    });
   }
 
   scanElement(): void {
